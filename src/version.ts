6c51ca267084f755ff3974c9ebaf9cdc60d5e328
leavesster <11785335+leavesster@users.noreply.github.com>
2021-01-08 16:18:48 +0800

    support relative path

--- src/version.ts
+++ src/version.ts
@@ -1,10 +1,14 @@
 import * as shell from "./shell";
+import * as path from "path";
 
 export function gitHash(): string {
     return shell.execSync("git rev-parse --short HEAD");
 }
 
-export function packageVersion(path: string): any {
-    const json = require(path);
+export function packageVersion(jsonPath: string): any {
+    if (!path.isAbsolute(jsonPath)) {
+        jsonPath = path.resolve(process.cwd(), jsonPath);
+    }
+    const json = require(jsonPath);
     return json.version;
 }
